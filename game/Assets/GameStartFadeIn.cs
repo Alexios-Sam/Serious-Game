1dcc5ff7f3a86b535aa9e92861c8441537a98033
Samuel <samuel.alexander.f@gmail.com>
2018-08-25 17:27:44 +0200

    Added Startup

---
+++ game/Assets/GameStartFadeIn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStartFadeIn : MonoBehaviour
+{
+    public Image Panel;
+    public float FadeDuration = 7.5f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Panel.enabled = true;
+	    Panel.CrossFadeAlpha(0.0f, FadeDuration, true);
+	}
+}
