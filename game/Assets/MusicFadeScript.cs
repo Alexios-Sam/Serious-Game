27ee2ef16665bdbf552de1b3b6e0131d503402a6
Samuel <samuel.alexander.f@gmail.com>
2018-08-26 11:34:23 +0200

    Added Startup Music

---
+++ game/Assets/MusicFadeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicFadeScript : MonoBehaviour
+{
+    public AudioClip StartUpAudioClip;
+    public AudioClip AmbientMusic;
+
+    public float VolumeOnStartUp = 0.75f;
+    public float VolumeOnAmbient = 0.5f;
+
+    private AudioSource sound;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    sound = GetComponent<AudioSource>();
+	    sound.volume = VolumeOnStartUp;
+        sound.clip = StartUpAudioClip;
+	}
+
+    private void Update()
+    {
+        if (!sound.isPlaying)
+        {
+            sound.volume = VolumeOnAmbient;
+            sound.clip = AmbientMusic;
+            sound.Play();
+            sound.loop = true;
+        }
+    }
+}
--- game/Assets/Our_Scripts/GameStartFadeIn.cs
+++ game/Assets/Our_Scripts/GameStartFadeIn.cs
@@ -5,13 +5,55 @@ using UnityEngine.UI;
 
 public class GameStartFadeIn : MonoBehaviour
 {
-    public Image Panel;
     public float FadeDuration = 10.0f;
+    public Color StartColor = Color.black;
+    public Color EndColor = Color.clear;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    Panel.enabled = true;
-	    Panel.CrossFadeAlpha(0.0f, FadeDuration, true);
-	}
+    public Incriment[] Incriments;
+    public float LastIncrimentSpeed = 2.0f;
+
+    [System.Serializable]
+    public class Incriment
+    {
+        [Range(0.0f, 1.0f)] public float IncrimentEnd = 0.5f;
+        public float IncrimentSpeed = 0.5f;
+    }
+
+    private int index;
+
+    private Image panel;
+    private float t;
+
+    // Use this for initialization
+    void Start()
+    {
+        panel = GetComponent<Image>();
+        panel.enabled = true;
+    }
+
+    private void Update()
+    {
+        panel.color = Color.Lerp(StartColor, EndColor, t);
+
+        if (Incriments.Length <= 0)
+        {
+            t += Time.deltaTime / FadeDuration;
+        }
+        else if (t < Incriments[index].IncrimentEnd)
+        {
+            t += Time.deltaTime / FadeDuration * Incriments[index].IncrimentSpeed;
+        }
+        else
+        {
+            if (Incriments.Length - 1 > index)
+            {
+                index++;
+                t += Time.deltaTime / FadeDuration * Incriments[index].IncrimentSpeed;
+            }
+            else
+            {
+                t += Time.deltaTime / FadeDuration * LastIncrimentSpeed;
+            }
+        }
+    }
 }
